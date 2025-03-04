0eaaed1d84ca57dc3384052ae5372435bccc5584
adiii3692 <adinair0206@gmail.com>
2025-03-04 10:59:59 -0500

    Implemented server side logic to create and update wishlist/recipe items

--- server/index.js
+++ server/index.js
@@ -130,6 +130,41 @@ app.post('/recipes/:id',async (request,response)=>{
     }
 })
 
+//Directory to update a recipe
+app.put('/recipes/:recipeId/:userId', async (request,response)=>{
+    try {
+        if (!request.body.title ||
+            !request.body.description ||
+            !request.body.content){
+                return response.json({message:"Please enter all fields",validated:false});
+            }
+        
+        const title = request.body.title
+        const description = request.body.description
+        const content = request.body.content
+        
+        const recipeId = parseInt(request.params.recipeId)
+        const userId = parseInt(request.params.userId)
+
+        const updateRecipe = await prisma.recipe.update({
+            where: {
+                authorId: userId,
+                id: recipeId
+            },
+            data: {
+                title: title,
+                description: description,
+                content: content
+            }
+        })
+
+        return response.status(200).json({message:`Updated recipe for userId: ${userId} and recipeId: ${recipeId}`,updatedRecipe:updateRecipe})
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    } 
+})
+
 //Directory to get wishlist
 app.get('/wishlist',async (request,response)=>{
     try {
@@ -145,3 +180,70 @@ app.get('/wishlist',async (request,response)=>{
     }
 })
 
+//Directory to make a wishlist
+app.post('/wishlist/:id',async (request,response)=>{
+    try {
+        if (!request.body.title ||
+            !request.body.description){
+                return response.json({message:"Please enter all fields",validated:false});
+            }
+        
+        const title = request.body.title
+        const description = request.body.description
+        const content = request.body.content
+        const imageUrl = request.body.imageUrl
+        
+        const userId = parseInt(request.params.id)
+
+        const createWishlist = await prisma.wishlist.create({
+            data: {
+                title: title,
+                description: description,
+                imageUrl: imageUrl,
+                content: content,
+                authorId: userId
+            }
+        })
+
+        return response.status(200).json({message:`Created a Wishlist Item for userId: ${userId}`,createdWishlist:createWishlist})
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    }
+})
+
+//Directory to update a wishlist
+app.put('/wishlist/:wishlistId/:userId', async (request,response)=>{
+    try {
+        if (!request.body.title ||
+            !request.body.description){
+                return response.json({message:"Please enter all fields",validated:false});
+            }
+        
+        const title = request.body.title
+        const description = request.body.description
+        const content = request.body.content
+        const imageUrl = request.body.imageUrl
+        
+        const wishlistId = parseInt(request.params.wishlistId)
+        const userId = parseInt(request.params.userId)
+
+        const updateWishlist = await prisma.wishlist.update({
+            where: {
+                authorId: userId,
+                id: wishlistId
+            },
+            data: {
+                title: title,
+                description: description,
+                content: content,
+                imageUrl: imageUrl
+            }
+        })
+
+        return response.status(200).json({message:`Updated wishlist for userId: ${userId} and wishlistId: ${wishlistId}`,updatedWishlist:updateWishlist})
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    } 
+})
