7ad0e93cc9323e7b3aa678b1b1b1be7440f4f7db
adiii3692 <adinair0206@gmail.com>
2025-03-12 10:42:05 -0400

    Implemented user login and custom playwright tests

---
+++ client/components/landingPage.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useRouter } from "next/navigation";
+
+const LandingPage = () =>{
+
+    const [username,setUsername] = useState<string>("");
+    const [password,setPassword] = useState<string>("");
+
+    const router = useRouter()
+
+    const logUser = async () => {
+        const user = {
+            username: username,
+            password: password
+        }
+
+        //The api endpoint to access
+        const url : string = `${process.env.NEXT_PUBLIC_API_URL}`+`login`
+        console.log('URL: ',url)
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                mode: "cors",
+                body: JSON.stringify(user)
+            });
+            if (!response.ok && response.status!=500) {
+              throw new Error(`Response status: ${response.status}`);
+            }
+            
+            const userJson = await response.json();
+            console.log(userJson);
+            if (userJson.validated){
+                console.log("User ID: ",userJson.foundUser.id)
+                localStorage.setItem("user_id",userJson.foundUser.id)
+                router.refresh()
+            }
+
+          } catch (error) {
+            console.log(error);
+          }
+    }
+
+    return(
+        <Card className="quicksand-bold w-[40%] h-[40%] my-8" data-testid="login-form">
+            <CardHeader className="h-[20%] text-center">
+                <CardTitle>Login</CardTitle>
+                <CardDescription>Kindly authenticate yourself</CardDescription>
+            </CardHeader>
+            <CardContent className="h-[75%]">
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="username">Username</Label>
+                            <Input id="username" name="username" placeholder="Username" onChange={(e)=>setUsername(e.target.value)}/>
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="password">Password</Label>
+                            <Input id="password" name="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex w-full justify-center">
+                <Button className="w-[50%]" onClick={logUser} type="submit">Login</Button>
+            </CardFooter>
+        </Card>
+    )
+}
+
+export default LandingPage;
---
+++ client/tests/login.test.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+import '../envConfig.ts'
+
+test('Landing Page and Login Form test', async ({ page }) => {
+  // LoginUrl
+  const loginUrl = process.env.NEXT_PUBLIC_CLIENT_URL
+  if (!loginUrl){
+    throw new Error('NEXT_PUBLIC_CLIENT_URL is not defined')
+  }
+
+  // Navigate to the login page
+  await page.goto(loginUrl);
+
+  // Check if login form exists
+  await expect(page.locator('[data-testid="login-form"]')).toBeVisible();
+
+  // Fill in the username and password fields
+  const username = process.env.NEXT_PUBLIC_TEST_USER
+  const password = process.env.NEXT_PUBLIC_TEST_PASS
+  if ((!username)||(!password)){
+    throw new Error('Username or Password is not defined')
+  }
+  await page.fill('input[name="username"]', username);
+  await page.fill('input[name="password"]', password);
+
+  // Click the login button
+  await page.click('button[type="submit"]');
+
+  // Check if home page after login has logout button
+  await expect(page.locator('[data-testid="logout"]')).toBeVisible();
+  // Check if cards are now visible
+  await expect(page.locator('[data-testid="card-component"]')).toBeVisible();
+});
